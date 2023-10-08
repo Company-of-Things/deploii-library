658f2f9d4206f7e2fe1195dc843b656e2c0542c2
Bissas <tobias@cot.as>
2023-10-08 17:05:18 +0200

    feat: Class declaration and WiFi connection

--- src/deploii.cpp
+++ src/deploii.cpp
@@ -1,1 +1,27 @@
 #include "deploii.h"
+
+Deploii::Deploii(const char *mcuID) {
+   _mcuID = mcuID;
+}
+
+void Deploii::connect_wifi(const char *ssid, const char *password) {
+
+   Serial.begin(9600);
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED)
+      delay(1000);
+
+   Serial.println("Wifi connected");
+
+   _ws.begin(DEPLOII_HOST, DEPLOII_PORT, DEPLOII_URL);
+   _ws.onEvent(nullptr);
+   _ws.setAuthorization(_mcuID, "");
+   _ws.setReconnectInterval(5000);
+   _connectedWiFi = true;
+}
+
+void Deploii::loop() {
+   if (_connectedWiFi) {
+      _ws.loop();
+   }
+}
--- src/deploii.h
+++ src/deploii.h
@@ -1,3 +1,27 @@
-#pragma once
+#ifndef Deploii_h
+#define Deploii_h
 
 #include "Arduino.h"
+
+#include <WiFi.h>
+#include <WebSocketsClient.h>
+
+#define DEPLOII_HOST "www.deploii.no"
+#define DEPLOII_PORT 5215
+#define DEPLOII_URL "/ws"
+
+class Deploii {
+ private:
+   WebSocketsClient _ws;
+   const char *_mcuID;
+   void eventHandler(WStype_t type, unsigned char *payload, size_t length);
+
+   bool _connectedWiFi = false;
+
+ public:
+   Deploii(const char *mcuID);
+   void loop();
+   void connect_wifi(const char *ssid, const char *password);
+};
+
+#endif
