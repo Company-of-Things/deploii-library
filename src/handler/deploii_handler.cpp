0aebfd956979e9e7a870186e12a4bca298326808
Bissas <tobias@cot.as>
2024-03-10 12:52:37 +0100

    fix: removed const from ssid

--- src/handler/deploii_handler.cpp
+++ src/handler/deploii_handler.cpp
@@ -6,7 +6,7 @@ void DeploiiHandler::send() {}
 void DeploiiHandler::loop() {}
 void DeploiiHandler::connect() {}
 void DeploiiHandler::connect(char* boardID,
-                             const char* ssid,
+                             char* ssid,
                              const char* pwd,
                              const char* host,
                              const int port,
