1caab5157c6c79b805e57180abd49e90a7172f17
Bissas <tobias@cot.as>
2024-09-01 13:24:35 +0200

    fix: send example

--- examples/deploii-send/deploii-send.ino
+++ examples/deploii-send/deploii-send.ino
@@ -7,9 +7,10 @@ int myDataArray[] = {1, 2, 3};
 
 void setup() {
    oi.connect("WiFi SSID", "WiFi Password");
+   oi.send("DataStream ID", myData);
+   oi.send("DataStream ID", myDataArray);
 }
 
 void loop() {
-   oi.send("DataStream ID", myData);
-   oi.send("DataStream ID", myDataArray);
+   oi.loop();
 }
