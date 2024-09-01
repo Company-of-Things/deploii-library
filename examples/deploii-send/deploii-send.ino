a82d04bd2484b6d5ec620576d0fcb30566e3160b
Bissas <tobias@cot.as>
2024-09-01 13:22:20 +0200

    example: send example

---
+++ examples/deploii-send/deploii-send.ino
@@ -0,0 +1,16 @@
+#include <Deploii.h>
+
+Deploii oi("Board ID", Medium::WiFi, Protocol::WebSockets);
+
+int myData = 10;
+int myDataArray[] = {1, 2, 3};
+
+void setup() {
+   oi.connect("WiFi SSID", "WiFi Password");
+}
+
+void loop() {
+   oi.send("DataStream ID", myData);
+   oi.send("DataStream ID", myDataArray);
+   delay(1000);
+}
