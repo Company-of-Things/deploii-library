6aa710363dfcfed7f969a5c11245309e7184e430
Aasmund Nørsett <aasmund.norsett@gmail.com>
2025-07-06 13:11:17 +0200

    fix: do not use relative includes

--- src/deploii.h
+++ src/deploii.h
@@ -13,7 +13,7 @@
 #include "Arduino.h"
 #include "deploii_certs.h"
 #include "deploii_config.h"
-#include "./handler/deploii_handler.h"
+#include "handler/deploii_handler.h"
 #include "MsgPack.h"
 #include <assert.h>
 
--- src/handler/deploii_handler.h
+++ src/handler/deploii_handler.h
@@ -9,11 +9,11 @@
  * Include correct microcontroller handler
  */
 #if defined(ESP32)
-#include "./esp32/deploii_handler_esp32.h"
+#include "handler/esp32/deploii_handler_esp32.h"
 #else
 #warning "The device you are using is currently not supported by Deploii."
 #define DEPLOII_DEFAULT
-#include "./default/deploii_handler_default.h"
+#include "handler/default/deploii_handler_default.h"
 #endif
 
 /*************************************************************************************/
